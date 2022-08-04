4f377f930f25870f6e54ae839b24aed4917cf94a
Hardik Parmar <hardik.parmar5@gmail.com>
2022-08-04 15:24:49 +0530

    create and list users in TS/express

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,15 +1,25 @@
 import  express,{Request, Response} from "express";
 import  User  from "../model/userModel";
+import {createUser, findUser, updateUser, deleteUser} from "../services/userService"
 
+//find without creating services
 
+// export const listUser = async (req:Request, res:Response) => {
+//     const users = await User.find({})
+//     res.json({users_are: users})
+// }
 
+//CRUD with the help of services
 
+// >> Find  <<
 export const listUser = async (req:Request, res:Response) => {
-    const users = await User.find({})
+    const users = await findUser({})
     res.json({users_are: users})
 }
 
-// export const createUSer = async(req:Request, res:Response) => {
-
-//     const newUser = await userModel.create({})
-// }
+// >> Add <<
+ export const newUser = async( req: Request, res: Response) => {
+    const addUser = await createUser({name: req.body.name, age: req.body.age}) 
+    res.json({added_user: addUser})
+ }
+// >> Update <<
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,12 @@
 import  express,{Request, Response} from "express";
 import { demo }from '../controller/demoController'
-import { listUser } from "../controller/userController";
+import { newUser,listUser } from "../controller/userController";
 const router = express.Router();
  
 
 router.get("/demo", demo);
-router.get("/user", listUser) 
+router.get("/list",listUser );
+router.post("/user", newUser) 
 
 
 export { router}
